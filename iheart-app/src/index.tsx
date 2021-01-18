1e8744c67d4855e3911af97c18cd0bb4e210fd00
aliajam <aliajam992@gmail.com>
2021-01-17 20:05:03 -0600

    refactor: add views and components

--- iheart-app/src/index.tsx
+++ iheart-app/src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// react library for routing
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Landing from './views/Landing';
+import Table from './views/Table';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' exact render={() => <Landing />} />
+      <Route path='/table' exact render={() => <Table />} />
+      <Redirect to='/' />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
