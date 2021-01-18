1f29cf1a86cf5fb2e2e60a0a14813ff5e31d6d2b
aliajam <aliajam992@gmail.com>
2021-01-17 21:53:48 -0600

    style: add btn style, add contents to Landing Page

--- iheart-app/src/views/Landing.tsx
+++ iheart-app/src/views/Landing.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footers/Footer';
 import Header from '../components/Headers/Header';
 
 const Landing: React.FC = () => {
   return (
-    <div>
+    <Fragment>
       <Header />
+      <div className='container'>
+        <h1 className='text-center'>Looking for music?</h1>
+        <button className='btn btn-primary ml-auto mr-auto'>
+          <Link to='/table'>Eplore iHeartRADIO</Link>
+        </button>
+      </div>
       <Footer />
-    </div>
+    </Fragment>
   );
 };
 
